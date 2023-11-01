4417982ea5f4df3ae135f6ee000d1ff02ed80baf
dima101130 <74977430+dima101130@users.noreply.github.com>
2023-11-01 20:39:49 +0500

    Enhanced move
    улутшеное управление

--- player.gd
+++ player.gd
@@ -1,31 +1,44 @@
 extends CharacterBody3D
 
+#movment
+@export var walk_speed := 6.0
+@export var sprint_speed := 10.0
 
-@export var speed = 5.0
-@export var jump_velocity = 4.5
+@export var gravity := 9.8
+@export var jump_power := 10.0
+@export var h_accel := 35.0
+@export var v_accel := 35.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-@export var gravity = 9.8
+var speed
+
+
+func _ready():
+	pass
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("left", "right", "forward", "backward")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+	# Horizontal movement
+	var horizontal_input := Vector3.ZERO
+	horizontal_input.z = Input.get_axis("forward", "backward")
+	horizontal_input.x = Input.get_axis("left", "right")
+	
+	if Input.is_action_pressed("sprint"):
+		speed = sprint_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
-
+		speed = walk_speed
+	
+	horizontal_input = horizontal_input.limit_length(1.0)
+	horizontal_input *= speed
+	horizontal_input.y = velocity.y
+	
+	velocity = velocity.move_toward(horizontal_input, h_accel * delta)
+	
+	# Vertical movement
+	velocity.y = move_toward(velocity.y, -gravity, v_accel * delta)
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_power
+		
+	
 	move_and_slide()
+	
